b9a109f5dad487fbb220d62d9983be034ccf940e
Armaan Sharma <sharmaarmaan.9871@gmail.com>
2025-12-21 16:03:32 +0530

    added the access and the constructor for the cache

--- cache.cpp
+++ cache.cpp
@@ -7,6 +7,7 @@ struct Cache_line{
     int time;
 };
 
+Cache_line* head =nullptr;
 class Cache{
     public:
     int cache_size;
@@ -23,7 +24,90 @@ class Cache{
 
     Cache(int cs,int bs,int assoc);
 
+    int get_set_i(int address){
+    int block_a=address/block_size;
+    return block_a%num_sets;
+    }
+    int get_tag(int address){
+        int block_a=address/block_size;
+        return block_a/num_sets;
+    }
+    Cache_line* access(int add){
+    int ind=get_set_i(add);
+    int taga=get_tag(add);
+    global_time++;
+   for(int i=0;i<associativity;i++){
+    if(sets[ind][i].tag==taga && sets[ind][i].valid ){
+        cout<<"hit"<<endl;
+        hits++;
+        sets[ind][i].time=global_time;
+        return &sets[ind][i];
+        }
+        else{
+           
+
+        }
+   }
+    cout<<"miss"<<endl;
+    misses++;
+    for(int i=0;i<associativity;i++){
+        if(!sets[ind][i].valid){
+          sets[ind][i].tag=taga;
+          sets[ind][i].valid=true;
+          sets[ind][i].time=global_time;
+          return &sets[ind][i]; 
+        }  
+    }
+    int min_t=INT_MAX;
+    for(int i=0;i<associativity;i++){
+        min_t=min(min_t,sets[ind][i].time); 
+    }
+    for(int i=0;i<associativity;i++){
+        if(sets[ind][i].time==min_t){
+            sets[ind][i].tag=taga;
+            sets[ind][i].time=global_time;
+            sets[ind][i].valid=true;
+             return &sets[ind][i];
+        } 
+    }
+    return nullptr;
+}\
 };
+
+Cache:: Cache(int cs,int bs,int assoc){
+    cache_size=cs;
+    block_size=bs;
+    associativity=assoc;
+
+    int num_blocks=cache_size/block_size;
+     num_sets=num_blocks/associativity;
+
+    sets.resize(num_sets,vector<Cache_line> (associativity));
+
+    for(int i=0;i<num_sets;i++){
+        for(int j=0;j<associativity;j++){
+            sets[i][j].valid=false;
+            sets[i][j].tag=-1;
+            sets[i][j].time=0;
+        }
+    }
+
+    global_time=0;
+    hits=0;
+    misses=0;
+}
+
 int main(){
 
+    Cache L1(64,8,2);
+    cout << "Sets: " << L1.num_sets << endl;
+
+    cout << "Addr 0  -> set " << L1.get_set_i(0)
+         << " tag " << L1.get_tag(0) << endl;
+
+    cout << "Addr 8  -> set " << L1.get_set_i(8)
+         << " tag " << L1.get_tag(8) << endl;
+
+    cout << "Addr 16 -> set " << L1.get_set_i(16)
+         << " tag " << L1.get_tag(16) << endl;
 }
